ee22df78abe24e091fb7a2804ce39b4d0814ddab
Nathan Berenger <nathanberenger@wanadoo.fr>
2018-07-08 19:45:56 +0200

    add interval to employeecategory + handler in employee forms

--- controllers/category.js
+++ controllers/category.js
@@ -1,49 +1,59 @@
-var models  = require('../models');
+var models = require('../models');
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     models.EmployeeCategory.findAll({
         order: [
             ['name', 'ASC']
         ]
     }).then(categories => {
         res.render('category/list.ejs',
-        {
-            categories: categories
-        });
+            {
+                categories: categories
+            });
     });
 });
 
-router.get('/add', function(req, res) {
+router.get('/add', function (req, res) {
     res.render('category/add.ejs');
 });
 
-router.post('/add', function(req, res) {
+router.post('/add', function (req, res) {
     models.EmployeeCategory.create({
-        name: req.body.name
+        name: req.body.name,
+        interval: { number: req.body.intervalnumber, type: req.body.intervaltype }
     }).then(category => {
         res.redirect('/employee/category');
     });
 });
 
-router.get('/:id(\\d+)/update', function(req, res) {
+router.get('/:id(\\d+)/update', function (req, res) {
     var id = req.params.id;
 
     models.EmployeeCategory.findById(id).then(category => {
-        res.render('category/update.ejs', {category: category});
+        res.render('category/update.ejs', {
+            category: category
+        });
     });
 });
 
-router.post('/:id(\\d+)/update', function(req, res) {
-    models.EmployeeCategory.update({
-        name: req.body.name,
-    }, {where: { id: req.params.id }}).then(category => {
-        res.redirect('/employee/category');
-    });
+router.post('/:id(\\d+)/update', function (req, res) {
+    models.EmployeeCategory.update(
+        {
+            name: req.body.name,
+            interval: { number: req.body.intervalnumber, type: req.body.intervaltype }
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then(category => {
+            res.redirect('/employee/category');
+        });
 });
 
-router.get('/:id(\\d+)/delete', function(req, res) {
+router.get('/:id(\\d+)/delete', function (req, res) {
     var id = req.params.id;
 
     models.EmployeeCategory.destroy({
@@ -68,27 +78,27 @@ router.get('/:id(\\d+)/delete', function(req, res) {
 
 
 
-router.get('/:id(\\d+)/availabilities', function(req, res) {
+router.get('/:id(\\d+)/availabilities', function (req, res) {
     var id = req.params.id;
     var year = (new Date()).getFullYear();
 
     res.redirect('/employee/' + id + '/availabilities/' + year);
 });
 
-router.get('/:id(\\d+)/availabilities/:year(\\d{4})', function(req, res) {
+router.get('/:id(\\d+)/availabilities/:year(\\d{4})', function (req, res) {
     var id = req.params.id;
     var year = req.params.year;
 
     models.Employee.findById(id).then(employee => {
         res.render('employee/list-yearly-availabilities.ejs',
-        {
-            employee: employee,
-            year: year
-        });
+            {
+                employee: employee,
+                year: year
+            });
     });
 });
 
-router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})', function(req, res) {
+router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})', function (req, res) {
     var id = req.params.id;
     var month = req.params.month;
     var year = req.params.year;
@@ -96,15 +106,15 @@ router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})', function(req
     var firstDate = new Date(year, parseInt(month) - 1, 1);
     var lastDate = new Date(year, parseInt(month), 0);
 
-    var firstDateFormated = firstDate.getFullYear() + '-' + (firstDate.getMonth()+1).toString().padStart(2, '0') + '-' + firstDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
-    var lastDateFormated = lastDate.getFullYear() + '-' + (lastDate.getMonth()+1).toString().padStart(2, '0') + '-' + lastDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
+    var firstDateFormated = firstDate.getFullYear() + '-' + (firstDate.getMonth() + 1).toString().padStart(2, '0') + '-' + firstDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
+    var lastDateFormated = lastDate.getFullYear() + '-' + (lastDate.getMonth() + 1).toString().padStart(2, '0') + '-' + lastDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
 
     models.Employee.findById(id).then(employee => {
         var availabilities = [];
         var promises = [];
 
-        for(var d=firstDate; d<=lastDate; d.setDate(d.getDate() + 1)) {
-            var date = d.getFullYear() + '-' + (d.getMonth()+1).toString().padStart(2, '0') + '-' + d.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
+        for (var d = firstDate; d <= lastDate; d.setDate(d.getDate() + 1)) {
+            var date = d.getFullYear() + '-' + (d.getMonth() + 1).toString().padStart(2, '0') + '-' + d.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
             var key = "defaultDay" + d.getDay();
             var type = (employee[key] !== null) ? employee[key] : "time.allday";
 
@@ -113,9 +123,10 @@ router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})', function(req
                 defaults: {
                     Employeeid: req.params.id,
                     type: type,
-                }}
-            ).spread(function(availability, created){
-                if (created){
+                }
+            }
+            ).spread(function (availability, created) {
+                if (created) {
                     availability.setEmployee(employee);
                 }
 
@@ -125,15 +136,15 @@ router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})', function(req
 
         Promise.all(promises).then(values => {
             res.render('employee/list-availabilities.ejs',
-            {
-                employee: employee,
-                availabilities: availabilities
-            });
+                {
+                    employee: employee,
+                    availabilities: availabilities
+                });
         });
     });
 });
 
-router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})/reset', function(req, res) {
+router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})/reset', function (req, res) {
     var id = req.params.id;
     var month = req.params.month;
     var year = req.params.year;
@@ -141,13 +152,13 @@ router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})/reset', functi
     var firstDate = new Date(year, parseInt(month) - 1, 1);
     var lastDate = new Date(year, parseInt(month), 0);
 
-    var firstDateFormated = firstDate.getFullYear() + '-' + (firstDate.getMonth()+1).toString().padStart(2, '0') + '-' + firstDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
-    var lastDateFormated = lastDate.getFullYear() + '-' + (lastDate.getMonth()+1).toString().padStart(2, '0') + '-' + lastDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
+    var firstDateFormated = firstDate.getFullYear() + '-' + (firstDate.getMonth() + 1).toString().padStart(2, '0') + '-' + firstDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
+    var lastDateFormated = lastDate.getFullYear() + '-' + (lastDate.getMonth() + 1).toString().padStart(2, '0') + '-' + lastDate.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
 
     var promises = [];
 
-    for(var d=firstDate; d<=lastDate; d.setDate(d.getDate() + 1)) {
-        var date = d.getFullYear() + '-' + (d.getMonth()+1).toString().padStart(2, '0') + '-' + d.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
+    for (var d = firstDate; d <= lastDate; d.setDate(d.getDate() + 1)) {
+        var date = d.getFullYear() + '-' + (d.getMonth() + 1).toString().padStart(2, '0') + '-' + d.getDate().toString().padStart(2, '0') + ' 00:00:00Z';
 
         promises.push(models.Availability.destroy({
             where: { Employeeid: req.params.id, day: date }
@@ -159,15 +170,15 @@ router.get('/:id(\\d+)/availabilities/:month(\\d{2}):year(\\d{4})/reset', functi
     });
 });
 
-router.post('/:id(\\d+)/availabilities/type/set', function(req, res) {
+router.post('/:id(\\d+)/availabilities/type/set', function (req, res) {
     var date = req.body.dateId + " 00:00:00Z";
 
     models.Availability.update({
         type: req.body.value
     },
-    {where: { Employeeid: req.params.id, day: date }}).then(employee => {
-        res.send("maj!")
-    });
+        { where: { Employeeid: req.params.id, day: date } }).then(employee => {
+            res.send("maj!")
+        });
 });
 
 module.exports = router;
